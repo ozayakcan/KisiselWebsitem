598d98b02750b66751354dd81c565df5f180e9d9
Özay Akcan <iletisim@ozayakcan.xyz>
2021-10-07 20:47:24 +0300

    robots.txt ve sitemap.xml artık php ile gösteriliyor.

---
+++ sayfalar/text/robots.php
@@ -0,0 +1,31 @@
+<?php
+$klasor = $_SERVER['DOCUMENT_ROOT'];
+$fonksiyonlar = $klasor . "/sayfalar/fonksiyonlar.php";
+include($fonksiyonlar);
+$func = new func;
+$domain = $func->domain();
+$protocol = $func->protocol();
+header('Content-Type: text/txt');
+?>
+User-agent: *
+Allow: /
+
+User-agent: *
+Disallow: /css/
+
+User-agent: *
+Disallow: /images/
+
+User-agent: *
+Disallow: /js/
+
+User-agent: *
+Disallow: /posts/
+
+User-agent: *
+Disallow: /post/
+
+User-agent: *
+Disallow: /composer/
+
+Sitemap: <?=$protocol.$domain;?>/sitemap.xml
