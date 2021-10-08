a2dfd4aaf8c2e65cb58bee939334a15cae655bd4
Özay Akcan <iletisim@ozayakcan.xyz>
2021-10-08 12:30:51 +0300

    Sitemap güncellendi.

--- sitemap.php
+++ sitemap.php
@@ -8,71 +8,54 @@ $domain = $func->domain();
 $protocol = $func->protocol();
 header('Content-Type: text/xml');
 ?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
-<url>
-<loc><?=$protocol.$domain;?>/</loc>
-<lastmod>2021-03-01T16:17:41+00:00</lastmod>
-<priority>1.00</priority>
-</url>
-<url>
-<loc><?=$protocol.$domain;?>/index.html</loc>
-<lastmod>2021-03-01T16:17:41+00:00</lastmod>
-<priority>0.80</priority>
-</url>
-<url>
-<loc><?=$protocol.$domain;?>/index.php</loc>
-<lastmod>2021-03-01T16:17:41+00:00</lastmod>
-<priority>0.80</priority>
-</url>
-<url>
-<loc><?=$protocol.$domain;?>/hakkimda.html</loc>
-<lastmod>2021-03-01T16:17:41+00:00</lastmod>
-<priority>0.80</priority>
-</url>
-<url>
-<loc><?=$protocol.$domain;?>/iletisim.html</loc>
-<lastmod>2021-03-01T16:17:41+00:00</lastmod>
-<priority>0.80</priority>
-</url>
-<url>
-<loc><?=$protocol.$domain;?>/linkler.html</loc>
-<lastmod>2021-03-01T16:17:41+00:00</lastmod>
-<priority>0.80</priority>
-</url>
-<url>
-<loc><?=$protocol.$domain;?>/calismalar.html</loc>
-<lastmod>2021-03-01T16:17:41+00:00</lastmod>
-<priority>0.80</priority>
-</url>
-<url>
-<loc><?=$protocol.$domain;?>/calismalar/web-tasarimin-temelleri.html</loc>
-<lastmod>2021-03-01T16:17:41+00:00</lastmod>
-<priority>0.80</priority>
-</url>
-<url>
-<loc><?=$protocol.$domain;?>/calismalar/programlama-temelleri.html</loc>
-<lastmod>2021-03-01T16:17:41+00:00</lastmod>
-<priority>0.80</priority>
-</url>
-<url>
-<loc><?=$protocol.$domain;?>/calismalar/grafik-ve-animasyon-I.html</loc>
-<lastmod>2021-03-01T16:17:41+00:00</lastmod>
-<priority>0.80</priority>
-</url>
-<url>
-<loc><?=$protocol.$domain;?>/calismalar/web-editoru.html</loc>
-<lastmod>2021-03-01T16:17:41+00:00</lastmod>
-<priority>0.80</priority>
-</url>
-<url>
-<loc><?=$protocol.$domain;?>/calismalar/gorsel-programlama-I.html</loc>
-<lastmod>2021-03-01T16:17:41+00:00</lastmod>
-<priority>0.80</priority>
-</url>
-<url>
-<loc><?=$protocol.$domain;?>/calismalar/grafik-ve-animasyon-II.html</loc>
-<lastmod>2021-03-01T16:17:41+00:00</lastmod>
-<priority>0.80</priority>
-</url>
+<urlset
+    xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+        http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+    <url>
+        <loc><?=$protocol.$domain;?>/</loc>
+        <lastmod>2021-10-08T09:23:19+00:00</lastmod>
+        <priority>1.00</priority>
+    </url>
+    <url>
+        <loc><?=$protocol.$domain;?>/hakkimda.html</loc>
+        <lastmod>2021-10-08T09:23:19+00:00</lastmod>
+        <priority>0.80</priority>
+    </url>
+    <url>
+        <loc><?=$protocol.$domain;?>/iletisim.html</loc>
+        <lastmod>2021-10-08T09:23:19+00:00</lastmod>
+        <priority>0.80</priority>
+    </url>
+    <url>
+        <loc><?=$protocol.$domain;?>/linkler.html</loc>
+        <lastmod>2021-10-08T09:23:19+00:00</lastmod>
+        <priority>0.80</priority>
+    </url>
+    <url>
+        <loc><?=$protocol.$domain;?>/calismalar/web-tasarimin-temelleri.html</loc>
+        <lastmod>2021-10-08T09:23:19+00:00</lastmod>
+        <priority>0.80</priority>
+    </url>
+    <url>
+        <loc><?=$protocol.$domain;?>/calismalar/programlama-temelleri.html</loc>
+        <lastmod>2021-10-08T09:23:19+00:00</lastmod>
+        <priority>0.80</priority>
+    </url>
+    <url>
+        <loc><?=$protocol.$domain;?>/calismalar/grafik-ve-animasyon-I.html</loc>
+        <lastmod>2021-10-08T09:23:19+00:00</lastmod>
+        <priority>0.80</priority>
+    </url>
+    <url>
+        <loc><?=$protocol.$domain;?>/calismalar/grafik-ve-animasyon-II.html</loc>
+        <lastmod>2021-10-08T09:23:19+00:00</lastmod>
+        <priority>0.80</priority>
+    </url>
+    <url>
+        <loc><?=$protocol.$domain;?>/calismalar.html</loc>
+        <lastmod>2021-10-08T09:23:19+00:00</lastmod>
+        <priority>0.80</priority>
+    </url>
 </urlset>
-
